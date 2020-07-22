cf31fb0219a994a23157af556265a39a4293e5a0
RobinHuang20 <robinpm@163.com>
2020-07-22 18:33:55 +0800

    robin2分支修改

--- EFCoreMigration/Program.cs
+++ EFCoreMigration/Program.cs
@@ -11,6 +11,7 @@ namespace EFCoreMigration
 {
     public class Program
     {
+        //robin2
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
